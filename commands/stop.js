4ab154d5ff64ebc52de7a8efda3b0df5be856416
s3719046 <37074069+s3719046@users.noreply.github.com>
2020-10-08 22:49:55 +1100

    plugdj stop

--- commands/stop.js
+++ commands/stop.js
@@ -2,5 +2,6 @@ exports.run = (client, message) => {
   if (!message.member.voice.channel) return message.channel.send(`${client.emotes.error} | You must be in a voice channel!`);
   if (!client.distube.isPlaying(message)) return message.channel.send(`${client.emotes.error} | There is nothing playing!`);
   client.distube.stop(message);
+  client.bot.close(false);
   message.channel.send(`${client.emotes.success} | Stopped!`);
 };
