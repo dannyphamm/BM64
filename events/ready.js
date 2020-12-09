62dba5451f109f1e678e094e9aa16f1e2c225754
s3719046 <37074069+s3719046@users.noreply.github.com>
2020-12-10 01:43:21 +1100

    update to stop spam

--- events/ready.js
+++ events/ready.js
@@ -6,19 +6,25 @@ module.exports = (client) => {
   function tick() {
     //get the mins of the current time
     var mins = new Date().getMinutes();
-    if (mins == "00" || mins % 10 == 00) {
-      fetch('https://meme-api.herokuapp.com/gimme').then(response => response.json()).then(data => {
-        const embed = new Discord.MessageEmbed()
-          .setColor('#0099ff')
-          .setURL(data.postLink)
-          .setAuthor(data.author)
-          .setTitle(data.title)
-          .setImage(data.url)
-          .setTimestamp()
-          .setFooter("r/" + data.subreddit + " | " + data.author);
-        client.channels.cache.find(i => i.name === 'daily-memes').send(embed);
+    var isSent = false;
+    if (!isSent) {
+      if (mins == "00" || mins % 10 == 00) {
+        fetch('https://meme-api.herokuapp.com/gimme').then(response => response.json()).then(data => {
+          const embed = new Discord.MessageEmbed()
+            .setColor('#0099ff')
+            .setURL(data.postLink)
+            .setAuthor(data.author)
+            .setTitle(data.title)
+            .setImage(data.url)
+            .setTimestamp()
+            .setFooter("r/" + data.subreddit + " | " + data.author);
+          client.channels.cache.find(i => i.name === 'daily-memes').send(embed);
+          isSent = true;
+        }
+        )
+      } else {
+        isSent = false;
       }
-      )
     }
   }
   setInterval(tick, 1000);
