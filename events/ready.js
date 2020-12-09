6794f008286e27ab9aedb605321718dc67fde69d
s3719046 <37074069+s3719046@users.noreply.github.com>
2020-12-10 01:31:12 +1100

    add auto meme

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,28 @@
-module.exports = () => {
+const fetch = require('node-fetch');
+const Discord = require('discord.js');
+module.exports = (client) => {
   console.log('BM64 is Online');
+
+  function tick() {
+    //get the mins of the current time
+    var mins = new Date().getMinutes();
+    if (mins == "00" || mins == "30") {
+      fetch('https://meme-api.herokuapp.com/gimme').then(response => response.json()).then(data => {
+        const embed = new Discord.MessageEmbed()
+          .setColor('#0099ff')
+          .setURL(data.postLink)
+          .setAuthor(data.author)
+          .setTitle(data.title)
+          .setImage(data.url)
+          .setTimestamp()
+          .setFooter("r/" + data.subreddit + " | " + data.author);
+        client.channels.cache.find(i => i.name === 'daily-memes').send(embed);
+      }
+      )
+    }
+  }
+  setInterval(tick, 1000);
 };
+
+
+
