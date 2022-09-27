00885f49223865be4bb3fd2cd572587bd465cfbf
s3719046 <37074069+s3719046@users.noreply.github.com>
2022-09-27 14:33:28 +1000

    remove console logs

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -2,9 +2,7 @@
 module.exports = {
     name: 'messageCreate',
     execute(message) {
-        console.log(message.channel.type)
         if(message.channel.type === 2) {
-            console.log(message)
             setTimeout(()=> {
                 message.delete()
             }, 900000)
