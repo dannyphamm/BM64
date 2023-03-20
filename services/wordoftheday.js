c0b1d7a1ce5123a9a7b5f9f8b535a5aec60f4b24
s3719046 <37074069+s3719046@users.noreply.github.com>
2023-03-21 02:18:28 +1100

    missing ..

--- services/wordoftheday.js
+++ services/wordoftheday.js
@@ -1,5 +1,5 @@
 const { WebhookClient } = require('discord.js');
-const config = require('./config.json');
+const config = require("../config.json")
 function createDefinitions(def) {
     const array = [];
 
