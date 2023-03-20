f888bfab52a3a79413759959028d1a74b6c5a40a
s3719046 <37074069+s3719046@users.noreply.github.com>
2023-03-21 02:12:26 +1100

    Update script actions so it uses subfolders

--- deploy-commands-actions.js
+++ deploy-commands-actions.js
@@ -2,16 +2,27 @@ const { REST, Routes } = require('discord.js');
 const fs = require('node:fs');
 const path = require('node:path');
 
-const commands = [];
-// Grab all the command files from the commands directory you created earlier
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+function getCommands(dir) {
+	const commands = [];
+	const files = fs.readdirSync(dir);
 
-// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	commands.push(command.data.toJSON());
+	for (const file of files) {
+		const filePath = path.join(dir, file);
+		const stat = fs.lstatSync(filePath);
+
+		if (stat.isDirectory()) {
+			commands.push(...getCommands(filePath));
+		} else if (file.endsWith('.js')) {
+			const command = require(filePath);
+			commands.push(command.data.toJSON());
+		}
+	}
+
+	return commands;
 }
+
+const commandsPath = path.join(__dirname, 'commands');
+const commands = getCommands(commandsPath);
 
 // Construct and prepare an instance of the REST module
 const rest = new REST({ version: '10' }).setToken(process.env.BM64_BETA_TOKEN);
@@ -20,7 +31,6 @@ const rest = new REST({ version: '10' }).setToken(process.env.BM64_BETA_TOKEN);
 (async () => {
 	try {
 		console.log(`Started refreshing ${commands.length} application (/) commands.`);
-
 		// The put method is used to fully refresh all commands in the guild with the current set
 		const data = await rest.put(
 			Routes.applicationCommands(process.env.BM64_BETA_CLIENTID),
