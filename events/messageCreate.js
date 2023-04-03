23420086f47a0282fe7b66ba5aaceaf85bef27e2
s3719046 <37074069+s3719046@users.noreply.github.com>
2023-04-03 23:07:08 +1000

    Fix deletion message from being auto deleted

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -2,7 +2,7 @@ const sentMessages = new Map();
 module.exports = {
   name: 'messageCreate',
   async execute(message) {
-    if (message.channel.type === 2) {
+    if (message.channel.type === 2 && message.content !== 'All messages sent in this text channel will automatically disappear after 15 minutes.') {
       setTimeout(() => {
         message.delete()
       }, 900000)
