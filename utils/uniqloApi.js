70d04eb16252ccddee7138a4ea7de4a55e4b56e5
s3719046 <37074069+s3719046@users.noreply.github.com>
2023-07-25 21:00:05 +1000

    fix config

--- utils/uniqloApi.js
+++ utils/uniqloApi.js
@@ -1,13 +1,15 @@
 const axios = require('axios');
-const config = require('../config');
+
 
 async function getUniqloItem(itemId) {
+    const config = require('../config');
     console.log(`${config.uniqloApiUrl}/products/${itemId}`)
     const response = await axios.get(`${config.uniqloApiUrl}/products/${itemId}`);
     return response.data.result.items[0];
 }
 // getlatestprice
 async function getLatestPrices(itemId) {
+    
     const item = await getUniqloItem(itemId);
     const basePrice = item.prices.base.value;
     const promoPrice = item.prices.promo ? item.prices.promo.value : null;
@@ -16,6 +18,7 @@ async function getLatestPrices(itemId) {
 
 
 const insertPrice = async (client, itemId, basePrice, promoPrice, title) => {
+    const config = require('../config');
     const uniqloCollection = client.mongodb.db.collection(config.mongodbDBUniqlo);
     const timestamp = new Date();
     await uniqloCollection.updateOne({ itemId, title }, {
