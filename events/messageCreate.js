23c7f3760972b854a77a0b3ba77f46ba1ff1bf45
Danny Pham <dannypha091@gmail.com>
2023-09-22 03:11:24 +1000

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -3,7 +3,14 @@ module.exports = {
   name: 'messageCreate',
   async execute(message) {
     if (config.mode !== 'DEV') {
-      if (message.channel.type === 2 && message.content !== 'All messages sent in this text channel will automatically disappear after 15 minutes.') {
+      if (message.channel.type === 2) {
+if(message.content == 'All messages sent in this text channel will automatically disappear after 15 minutes.') return;
+if(message.embeds){
+if (message.embeds[0].title == 'Recently Played')
+return;
+}
+
+
         setTimeout(() => {
           message.delete()
         }, 900000)
