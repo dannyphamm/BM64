2f43c461643106ef213c17a409c86b1dba946e3c
s3719046 <37074069+s3719046@users.noreply.github.com>
2023-10-03 21:32:27 +1100

    reddit fix

--- services/redditmemes.js
+++ services/redditmemes.js
@@ -5,33 +5,32 @@ const redditMemesService = async (client) => {
     const fetch = await import('node-fetch');
 
     await fetch.default('https://meme-api.com/gimme')
-        .then(response => response.json())
+        .then(response => {
+            if(response.status !== 200) return
+            return response.json()
+        })
         .then(async data => {
-            if (data) {
-                const exampleEmbed = {
-                    title: data.title,
+            if (!data) return;
+            const exampleEmbed = {
+                title: data.title,
+                url: data.url,
+                image: {
                     url: data.url,
-                    image: {
-                        url: data.url,
-                    },
-                    color: 0x7289da,
-                    timestamp: new Date().toISOString(),
-                    footer: {
-                        text: 'r/' + data.subreddit + ' • ' + data.author + ' • ' + 'Powered by BM64',
-                    }
-                };
-                const channel = await client.channels.cache.find(c => c.name === '😂daily-memes😂');
-                if (!channel) return;
-                const webhooks = await channel.fetchWebhooks();
-                if (webhooks.size === 0) return;
-                const webhook = new WebhookClient({ id: webhooks.first().id, token: webhooks.first().token });
-                webhook.send({
-                    embeds: [exampleEmbed],
-                });
-            } else {
-                error('No meme data available')
-            }
-
+                },
+                color: 0x7289da,
+                timestamp: new Date().toISOString(),
+                footer: {
+                    text: 'r/' + data.subreddit + ' • ' + data.author + ' • ' + 'Powered by BM64',
+                }
+            };
+            const channel = await client.channels.cache.find(c => c.name === '😂daily-memes😂');
+            if (!channel) return;
+            const webhooks = await channel.fetchWebhooks();
+            if (webhooks.size === 0) return;
+            const webhook = new WebhookClient({ id: webhooks.first().id, token: webhooks.first().token });
+            webhook.send({
+                embeds: [exampleEmbed],
+            });
         })
         .catch(e => {
             error('Error fetching meme:', e);
