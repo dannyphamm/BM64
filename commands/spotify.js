9360f9751bcfa88f5291eecc05e2e9a3b1f36234
s3719046 <37074069+s3719046@users.noreply.github.com>
2023-11-06 21:22:41 +1100

    socket

--- commands/spotify.js
+++ commands/spotify.js
@@ -30,7 +30,7 @@ module.exports = {
         // return interaction.reply(`
 		// 	Sent		
 		// 	`);
-        await socketIO.emit('playMusic')
+        await socketIO().emit('playMusic')
         return interaction.reply(`Sent`);
     },
 };
