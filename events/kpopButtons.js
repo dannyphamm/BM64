d001b0393fe2106670655c97be8b3f92add9cb09
Danny Pham <37074069+dannyphamm@users.noreply.github.com>
2023-12-11 00:51:48 +1100

    Refactor kpopButtons.js: Fix indentation and add loadSpotify() function calls

--- events/kpopButtons.js
+++ events/kpopButtons.js
@@ -6,20 +6,21 @@ const config = require('../config.json');
 module.exports = {
     name: 'interactionCreate',
     async execute(interaction) {
-        const {client} = interaction;
+        const { client } = interaction;
         // v14 interaction.type == InteractionType.ApplicationCommandAutocomplete
         if (interaction.type == InteractionType.MessageComponent) {
             if (!interaction.isButton()) return
             log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
             if (interaction.customId === 'skip') {
                 socketIO().emit('skipMusic');
+                loadSpotify(client, true)
                 await interaction.reply({ content: 'Skipped!', ephemeral: true });
             }
 
             if (interaction.customId === 'remove') {
                 const spotifyApi = await spotify();
                 const currentSong = await spotifyApi.getMyCurrentPlayingTrack();
-                if(currentSong.body.currently_playing_type !== 'track') return await interaction.reply({ content: 'Cannot remove. An ad is playing!', ephemeral: true });
+                if (currentSong.body.currently_playing_type !== 'track') return await interaction.reply({ content: 'Cannot remove. An ad is playing!', ephemeral: true });
                 const playlist = currentSong.body.context.uri.split(':')[2];
                 await spotifyApi.removeTracksFromPlaylist(
                     playlist,
@@ -28,15 +29,15 @@ module.exports = {
                 await misamo.deleteOne({ uri: `spotify:track:${currentSong.body.item.id}` });
                 socketIO().emit('skipMusic');
                 // delay and add loadSpotify
-                
-                loadSpotify(client,true)
+
+                loadSpotify(client, true)
 
                 await interaction.reply({ content: 'Deleted!', ephemeral: true });
             }
 
             if (interaction.customId === 'reset') {
                 socketIO().emit('playMusic');
-                loadSpotify(client,true)
+                loadSpotify(client, true)
                 await interaction.reply({ content: 'Reset Triggered', ephemeral: true });
             }
         }
