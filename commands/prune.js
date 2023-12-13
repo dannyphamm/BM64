16c84f0e3e3d799c9ba8058a1729d09418585148
Danny Pham <37074069+dannyphamm@users.noreply.github.com>
2023-12-13 19:37:47 +1100

    asd

--- commands/prune.js
+++ commands/prune.js
@@ -7,7 +7,6 @@ module.exports = {
 		.addIntegerOption(option => option.setName('amount').setDescription('Number of messages to prune')),
 	async execute(interaction) {
 		const amount = interaction.options.getInteger('amount');
-
 		if (amount < 1 || amount > 99) {
 			return interaction.reply({ content: 'You need to input a number between 1 and 99.', ephemeral: true });
 		}
