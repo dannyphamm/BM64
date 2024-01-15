e42b4d49db9f4cab86d9b236e6c02a81af9ac0b1
Danny Pham <37074069+dannyphamm@users.noreply.github.com>
2024-01-16 01:34:21 +1100

    Add loadSpotify function to misamoPrioRequest.js

--- events/misamoPrioRequest.js
+++ events/misamoPrioRequest.js
@@ -2,6 +2,7 @@ const config = require('../config.json');
 const { spotify, getAllPlaylistSongs } = require('../utils/spotify.js');
 const { error, log } = require('../utils/utils');
 const { socketIO } = require('../utils/socket.js');
+const { loadSpotify } = require('../services/spotifyStatus.js');
 module.exports = {
     name: 'messageCreate',
     async execute(message) {
@@ -11,17 +12,17 @@ module.exports = {
         if (message.channel.id === config.misamoVoiceChannel) {
             const song = message.content;
 
-            // Delete the message
-            await message.delete();
-
             // Emit the addSongToQueue event
             await socketIO().then(async (socket) => {
                 const result = await socket.timeout(10000).emitWithAck('addSongToQueue', song);
                 if (result) {
                     await message.react('✅').catch((e) => error(e));
+                    // get queue
+                    await loadSpotify(client, false);
                 } else {
                     await message.react('❌').catch((e) => error(e));
                 }
+                
             })
         }
     }
