c66218241c3c7c01301a7b0ab59858f71df88989
Danny Pham <37074069+dannyphamm@users.noreply.github.com>
2024-03-16 01:02:26 +1100

    Add log import to misamoPrioRequest.js

--- events/misamoPrioRequest.js
+++ events/misamoPrioRequest.js
@@ -2,7 +2,7 @@ const { error } = require('../utils/utils');
 const { loadSpotify } = require('../services/spotifyStatus');
 const { socketIO } = require('../utils/socket.js');
 const config = require('../config.json');
-
+const { error, log } = require('../utils/utils');
 const queue = [];
 
 module.exports = {
