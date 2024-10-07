c55db48ab81e03dd4a301d7e82c814db07f5351b
Danny Pham <37074069+dannyphamm@users.noreply.github.com>
2024-10-07 23:21:15 +1100

    fix for user leaving

--- events/afkkick.js
+++ events/afkkick.js
@@ -9,12 +9,16 @@ module.exports = {
             const oldChannel = oldState.channel;
             if (newChannel && newChannel.id === '360023189205549075' && newState.channelId !== oldState.channelId) {
                 log(`${member.user.tag} entered afk.`)
-                setTimeout(() => {
+                const timeout = setTimeout(() => {
                     if (member.voice.channel && member.voice.channel.id === '360023189205549075') {
                         member.voice.setChannel(null);
                         log(`Kicked ${member.user.tag} from AFK channel after 3 hours`);
                     }
                 }, 3 * 60 * 60 * 1000);
+                member.voice.on('channelLeave', () => {
+                    clearTimeout(timeout);
+                    log(`${member.user.tag} left the AFK channel.`);
+                });
             }
         }
     },
