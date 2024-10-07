8fe8cd01cf830ad85b88180a2cbc141b438825ab
Danny Pham <37074069+dannyphamm@users.noreply.github.com>
2024-10-07 23:15:58 +1100

    afk

---
+++ events/afkkick.js
@@ -0,0 +1,21 @@
+const { log, error } = require('../utils/utils')
+const config = require('../config.json')
+module.exports = {
+    name: 'voiceStateUpdate',
+    async execute(oldState, newState) {
+        if (config.mode !== 'DEV') {
+            const member = newState.member;
+            const newChannel = newState.channel;
+            const oldChannel = oldState.channel;
+            if (newChannel && newChannel.id === '360023189205549075' && newState.channelId !== oldState.channelId) {
+                log(`${member.user.tag} entered afk.`)
+                setTimeout(() => {
+                    if (member.voice.channel && member.voice.channel.id === '360023189205549075') {
+                        member.voice.setChannel(null);
+                        log(`Kicked ${member.user.tag} from AFK channel after 3 hours`);
+                    }
+                }, 3 * 60 * 60 * 1000);
+            }
+        }
+    },
+};
