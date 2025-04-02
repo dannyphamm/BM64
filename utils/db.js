6949053ae13d64ddb0def7c0bdfd4f237e7116ab
Danny Pham <37074069+dannyphamm@users.noreply.github.com>
2025-04-03 02:27:08 +1100

    Refactor MongoDB connection implementation in db.js
    - Replace MongoClient with Mongoose for improved connection handling
    - Introduce retry logic for connection attempts and handle disconnection events
    - Streamline connection options for better performance and reliability

--- utils/db.js
+++ utils/db.js
@@ -1,39 +1,39 @@
-const { MongoClient } = require('mongodb');
+const mongoose = require('mongoose');
 const config = require('../config');
 const { log, error } = require('./utils');
 
 const MONGODB_URI = config.mongodbURI;
 const MONGODB_DB_NAME = config.mongodbDBName;
 
-class MongoConnection {
-  constructor() {
-    if (MongoConnection.instance) {
-      return MongoConnection.instance;
-    }
-
-    this.client = null;
-    this.db = null;
-    MongoConnection.instance = this;
-  }
-
-  async connect() {
-    try {
-      this.client = new MongoClient(MONGODB_URI, {
+// Basic retry logic for free tier
+const connectWithRetry = async (uri) => {
+    const options = {
         retryWrites: true,
         w: 'majority',
         useUnifiedTopology: true,
         serverSelectionTimeoutMS: 5000,
-        socketTimeoutMS: 30000,  
-      });
+        socketTimeoutMS: 30000,
+    };
 
-      await this.client.connect();
-      this.db = this.client.db(MONGODB_DB_NAME);
-      log('Connected to MongoDB');
-      
+    try {
+        await mongoose.connect(uri, options);
+        log('Connected to MongoDB');
     } catch (err) {
-      error('Error connecting to MongoDB:', err);
+        error('MongoDB connection error:', err);
+        // Wait 5 seconds and try again
+        setTimeout(() => connectWithRetry(uri), 5000);
     }
-  }
-}
+};
 
-module.exports = new MongoConnection();
+// Handle disconnection events
+mongoose.connection.on('disconnected', () => {
+    log('MongoDB disconnected, attempting to reconnect...');
+    connectWithRetry(MONGODB_URI);
+});
+
+mongoose.connection.on('error', (err) => {
+    error('MongoDB error:', err);
+    // If connection was lost, mongoose will try to reconnect automatically
+});
+
+module.exports = mongoose.connection;
